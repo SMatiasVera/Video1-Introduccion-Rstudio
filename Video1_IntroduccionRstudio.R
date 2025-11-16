f1d8fd264c31e6abbda9e408ea20535230893635
Sergio Matias Vera <smvera1899@gmail.com>
2025-11-16 15:21:46 -0600

    Add files via upload

---
+++ Video1_IntroduccionRstudio.R
@@ -0,0 +1,39 @@
+#Video 1: Introducción a Rstudio
+#Creando nuestro primer script
+
+
+#Paso1: Identificar ruta de trabajo
+#¿En qué carpeta estoy?
+getwd()
+
+#¿Cómo moverme de carpeta?
+setwd("/Users/sergioverap/Rstudio/Video3")
+
+
+#Paso2: Crear objetos en Rstudio
+#Ejemplo: Valor numérico
+
+Muestras <- c(1,3,5,6,7,8)
+Muestras
+
+#Ejemplo: Caracteres
+
+Nombres <- c("Alex","María", "Alberto")
+Nombres
+
+#Ejemplo: Nombrando objetos
+
+b1221 <- c(1,2,3)
+
+#Paso3: Instalando paqueterías
+
+install.packages("ggplot2")
+
+#Activar paquetería
+
+library(ggplot2)
+library(ggvenn)
+
+#Suscribete al canal :D
+
+
